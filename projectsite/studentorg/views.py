752f8a559390a342d248154fd5364e8a72c98c22
sude-marco-dev <202380284@psu.palawan.edu.ph>
2026-03-03 03:51:51 +0800

    Part 2: Dashboard Statistics

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.db.models import Q
+from django.utils import timezone
+from django.db.models import Q
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from studentorg.models import Organization, OrgMember, Student, College, Program
@@ -10,6 +12,23 @@ class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["total_students"] = Student.objects.count()
+
+        today = timezone.now().date()
+        count = (
+            OrgMember.objects.filter(
+                date_joined__year=today.year
+            )
+            .values("student")
+            .distinct()
+            .count()
+        )
+
+        context["students_joined_this_year"] = count
+        return context
 
 class OrganizationList(ListView):
     model = Organization
